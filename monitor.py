cec444cb130b30040947b1d625e24bdb9c828e12
Ben Sgro <sk@mr-sk.com>
2013-05-06 21:54:52 -0400

    Added multiple worker support

--- monitor.py
+++ monitor.py
@@ -2,7 +2,9 @@
 # -----------------------------------------------------------------------------
 # monitor.py
 #
-# Shoutz: All the BTC & LTC guys on the forums: MKEGuy, zif, Delarock. 
+# Shoutz: All the BTC & LTC guys on the forums: zif, Delarock. 
+#         efnet #innercircle, freenode #give-me-ltc
+# Fuckz: MKEGuy <-- scammer!
 #
 # Author: sk / sk@mr-sk.com
 # -----------------------------------------------------------------------------
@@ -25,6 +27,7 @@ if os.path.exists("workers.p"):
 giveMeLTCAPI = 'https://give-me-ltc.com/api?api_key='
 req    = urllib2.Request(giveMeLTCAPI + config.poolKey)
 opener = urllib2.build_opener()
+
 for parentKey, subDict in json.loads(opener.open(req).read())['workers'].iteritems():
    if 'last_share_timestamp' in subDict:
        inmemoryDict[parentKey] = subDict['hashrate']
@@ -36,13 +39,17 @@ if len(loadedDict.keys())< 1:
 
 # Two dictionaries with values, compare the hash values in each
 # and make sure they don't both fall below the threshold. 
-for lKey, lHash in loadedDict.iteritems():
-    if inmemoryDict[lKey] <= config.hashMin and lHash <= config.hashMin:     
-        print "[!] Issue found with worker %s, sending alert" % lKey
-        sns = boto.connect_sns()
-        msg         = "Worker %s is below %s" % (lKey, config.hashMin)
-        mytopic_arn = config.snsTopic
-        res = sns.publish(mytopic_arn, msg, '')
+dictIter = loadedDict.iteritems()
+for wName, wHash in config.workerDict.iteritems():
+    for lKey, lHash in dictIter:
+        if inmemoryDict[lKey]:
+            if int(lHash) < wHash:     
+                print "[!] Issue found with worker %s, sending alert" % lKey
+                sns = boto.connect_sns()
+                msg         = "Worker %s is below %s" % (lKey, wHash)
+                mytopic_arn = config.snsTopic
+                res = sns.publish(mytopic_arn, msg, '')
+        break
 
 pickle.dump(inmemoryDict, open("workers.p", "wb"))    
-print "Complete\n"
+print "[x] Complete\n"
